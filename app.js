1bc1843bb92b0b68acccccddd017c2417a1e7c8a
Joker <admin@e.mail.de>
2023-04-01 23:06:12 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// import translate from "google-translate-api-x";
+
+// const res = await translate("dull", { from: "en", to: "de" }).then((res) => {
+//    console.log(res);
+//    const body = document.querySelector("body");
+//    body.textContent = res;
+// });
+
+// console.log("response", res);
+
+console.log("test");
+
+console.log(typeof require);
